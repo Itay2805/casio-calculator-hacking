fb3b6f6a419153a110829803ac197fd792071c84
Itay Almog <itay2828@gmail.com>
2022-12-19 01:08:45 +0200

    WIP for lifting

--- nxu8100/architecture.py
+++ nxu8100/architecture.py
@@ -4,6 +4,10 @@ import codecs
 from .disasm import decode_instruction, DecodedInstruction, PushRegisterList, PopRegisterList
 
 
+def _prepare_flags():
+    pass
+
+
 class Nxu8100(Architecture):
     name = "nx-u8/100"
     endianness = Endianness.LittleEndian
@@ -53,10 +57,10 @@ class Nxu8100(Architecture):
         'SP': RegisterInfo('SP', 2),
         'EA': RegisterInfo('EA', 2),
 
-        'CSR': RegisterInfo('CSR', 1),
+        'PC': RegisterInfo('PC', 3),
+        'LR': RegisterInfo('LR', 3),
 
-        'PC': RegisterInfo('PC', 2),
-        'LR': RegisterInfo('LR', 2),
+        'DSR': RegisterInfo('DSR', 1),
 
         'PSW': RegisterInfo('PSW', 1),        
     }
@@ -65,7 +69,6 @@ class Nxu8100(Architecture):
         'Z', # Zero
         'S', # Sign
         'OV', # Overflow
-        'MIE', # Master Interrupt Enable
         'HC' # Half carry
     }
     flag_roles = {
@@ -73,13 +76,24 @@ class Nxu8100(Architecture):
         'Z': FlagRole.ZeroFlagRole,
         'S': FlagRole.NegativeSignFlagRole,
         'OV': FlagRole.OverflowFlagRole,
-        'MIE': FlagRole.SpecialFlagRole,
         'HC': FlagRole.HalfCarryFlagRole,
     }
     flag_write_types = [
-        ''
+        '',
+        'CZSOVHC',
+        'ZS',
+        'C',
+        'CZSHC',
+        'Z',
+        'ZSOVHC'
     ]
     flags_written_by_flag_write_type = {
+        'CZSOVHC': ['C', 'Z', 'S', 'OV', 'HC'],
+        'ZS': ['Z', 'S'],
+        'C': ['C'],
+        'CZSHC': ['C', 'Z', 'S', 'HC'],
+        'Z': ['Z'],
+        'ZSOVHC': ['Z', 'S', 'OV', 'HC'],
     }
     flags_required_for_flag_condition = {
         LowLevelILFlagCondition.LLFC_UGE: ['C'],
@@ -331,7 +345,377 @@ class Nxu8100(Architecture):
             return (tokens, decoded.length)
 
     def get_instruction_low_level_il(self, data: bytes, addr: int, il: lowlevelil.LowLevelILFunction) -> Optional[int]:
-        return None
+        decoded = decode_instruction(data, addr)
+        if decoded:
+            inst = decoded.instruction
+            oprs = decoded.operands
+
+            def full_adr(sr, adr):
+                return il.or_expr(3, il.shift_left(3, sr, il.const(1, 16)), adr)
+
+            def dsr_prefix(adr):
+                if decoded.dsr_format == 'Rd':
+                    r = il.reg(1, f'R{decoded.dsr_operand}')
+                    il.append(il.set_reg(1, 'DSR', r))
+                elif decoded.dsr_format == '#imm8':
+                    r = il.const(1, decoded.dsr_operand)
+                    il.append(il.set_reg(1, 'DSR', r))
+                elif decoded.dsr_format == 'DSR':
+                    r = il.reg(1, 'DSR')
+                else:
+                    return adr
+                return full_adr(r, adr)
+
+            flags = ''.join(inst.flags)
+
+            _ = None
+            if decoded.mnemonic == '':
+                assert False
+
+            if decoded.mnemonic == 'ADD':
+                if inst.first_operand == 'ERn':
+                    sz = 2
+                    opr1 = il.reg(2, f'ER{oprs[0]}')
+                    if inst.second_operand == 'ERm':
+                        opr2 = il.reg(2, f'ER{oprs[1]}')
+                    else:
+                        opr2 = il.const(1, oprs[1])
+                elif inst.first_operand == 'Rn':
+                    sz = 1
+                    opr1 = il.reg(2, f'R{oprs[0]}')
+                    if inst.second_operand == 'Rm':
+                        opr2 = il.reg(2, f'R{oprs[1]}')
+                    else:
+                        opr2 = il.const(1, oprs[1])
+                elif inst.first_operand == 'SP':
+                    sz = 2
+                    opr1 = il.reg(2, 'SP')
+                    opr2 = il.const(1, oprs[1])
+                else:
+                    assert False
+                _ = il.set_reg(sz, f'R{oprs[0]}', il.add(sz, opr1, opr2, flags))
+
+            elif decoded.mnemonic == 'ADDC':
+                opr1 = il.reg(1, f'R{oprs[1]}')
+                if inst.second_operand == 'Rm':
+                    opr2 = il.const(1, f'R{oprs[1]}')
+                else:
+                    opr2 = il.const(1, oprs[1])
+                _ = il.set_reg(1, f'R{oprs[1]}', il.add_carry(1, opr1, opr2, il.flag('C')))
+
+            elif decoded.mnemonic == 'AND':
+                opr1 = il.reg(1, f'R{oprs[1]}')
+                if inst.second_operand == 'Rm':
+                    opr2 = il.const(1, f'R{oprs[1]}')
+                else:
+                    opr2 = il.const(1, oprs[1])
+                _ = il.set_reg(1, f'R{oprs[1]}', il.and_expr(1, opr1, opr2))
+
+            elif decoded.mnemonic == 'B':
+                if inst.first_operand == 'ERn':
+                    # preserve the segment
+                    addr &= 0xF0000
+                    r = il.reg(2, f'ER{oprs[0]}')
+                    adr = il.or_expr(3, addr, r)
+                    _ = il.jump(adr)
+                else:
+                    _ = il.jump(il.const(3, oprs[0]))
+
+            elif decoded.mnemonic in [
+                'BGE',
+                'BLT',
+                'BGT',
+                'BLE',
+                'BGES',
+                'BLTS',
+                'BGTS',
+                'BLES',
+                'BNE',
+                'BEQ',
+                'BNV',
+                'BOV',
+                'BPS',
+                'BNS',
+            ]:
+                target = oprs[0]
+                next_pc = addr + decoded.length
+                trueLabel = il.get_label_for_address(self, target)
+                falseLabel= il.get_label_for_address(self, next_pc)
+
+                conds = {
+                    'BGE': LowLevelILFlagCondition.LLFC_UGE,
+                    'BLT': LowLevelILFlagCondition.LLFC_ULT,
+                    'BGT': LowLevelILFlagCondition.LLFC_UGT,
+                    'BLE': LowLevelILFlagCondition.LLFC_ULE,
+                    'BGES': LowLevelILFlagCondition.LLFC_SGE,
+                    'BLTS': LowLevelILFlagCondition.LLFC_SLT,
+                    'BGTS': LowLevelILFlagCondition.LLFC_SGT,
+                    'BLES': LowLevelILFlagCondition.LLFC_SLE,
+                    'BNE': LowLevelILFlagCondition.LLFC_NE,
+                    'BEQ': LowLevelILFlagCondition.LLFC_E,
+                    'BNV': LowLevelILFlagCondition.LLFC_NO,
+                    'BOV': LowLevelILFlagCondition.LLFC_O,
+                    'BPS': LowLevelILFlagCondition.LLFC_POS,
+                    'BNS': LowLevelILFlagCondition.LLFC_NEG,
+                }
+                cond = il.flag_condition(conds[inst.mnemonic])
+
+                if trueLabel and falseLabel:
+                    _ = il.if_expr(cond, trueLabel, falseLabel)
+
+                else:
+                    trueCode = LowLevelILLabel()
+                    falseCode = LowLevelILLabel()
+
+                    if trueLabel:
+                        il.append(il.if_expr(cond, trueLabel, falseCode))
+                        il.mark_label(falseCode)
+                        il.append(il.jump(il.const_pointer(2, next_pc)))
+
+                    elif falseLabel:
+                        il.append(il.if_expr(cond, trueCode, falseLabel))
+                        il.mark_label(trueCode)
+                        il.append(il.jump(il.const_pointer(2, target)))
+                        
+                    else:
+                        il.append(il.if_expr(cond, trueCode, falseCode))
+                        il.mark_label(trueCode)
+                        il.append(il.jump(il.const_pointer(2, target)))
+                        il.mark_label(falseCode)
+                        il.append(il.jump(il.const_pointer(2, next_pc)))
+
+            elif decoded.mnemonic == 'BAL':
+                target = oprs[0]
+                label = il.get_label_for_address(self, target)
+                if label:
+                    _ = il.goto(label)
+                else:
+                    _ = il.jump(il.const_pointer(2, target))
+
+            elif decoded.mnemonic == 'BL':                
+                if inst.first_operand == 'ERn':
+                    # preserve the segment
+                    addr &= 0xF0000
+                    r = il.reg(2, f'ER{oprs[0]}')
+                    adr = il.or_expr(3, addr, r)
+                    _ = il.call(adr)
+                else:
+                    _ = il.call(il.const_pointer(3, oprs[0]))
+
+            elif decoded.mnemonic == 'BRK':
+                _ = il.breakpoint()
+            
+            elif decoded.mnemonic == 'CMP':
+                if inst.first_operand == 'ERn':
+                    sz = 2
+                    opr1 = il.reg(2, f'ER{oprs[0]}')
+                    if inst.second_operand == 'ERm':
+                        opr2 = il.reg(2, f'ER{oprs[1]}')
+                    else:
+                        opr2 = il.const(1, oprs[1])
+                elif inst.first_operand == 'Rn':
+                    sz = 1
+                    opr1 = il.reg(2, f'R{oprs[0]}')
+                    if inst.second_operand == 'Rm':
+                        opr2 = il.reg(2, f'R{oprs[1]}')
+                    else:
+                        opr2 = il.const(1, oprs[1])
+                else:
+                    assert False
+
+                # TODO: update flags
+                _ = il.sub(sz, opr1, opr2, flags)
+
+            elif decoded.mnemonic == 'CMPC':
+                opr1 = il.reg(1, f'R{oprs[1]}')
+                if inst.second_operand == 'Rm':
+                    opr2 = il.const(1, f'R{oprs[1]}')
+                else:
+                    opr2 = il.const(1, oprs[1])
+                _ = il.sub_borrow(1, opr1, opr2, il.flag('C'), flags)
+    
+            elif decoded.mnemonic == 'CPLC':
+                _ = il.set_flag('C', il.not_expr(1, il.flag('C')))
+            
+            elif decoded.mnemonic == 'DEC':
+                adr = il.reg(2, 'EA')
+                adr = dsr_prefix(adr)
+                _ = il.store(1, adr, il.sub(1, il.load(1, adr), il.const(1, 1)))
+
+            elif decoded.mnemonic == 'DIV':
+                opr1 = il.reg(2, f'ER{oprs[0]}')
+                opr2 = il.reg(1, f'R{oprs[1]}')
+                d = il.div_unsigned(2, opr1, opr2)
+                m = il.mod_unsigned(1, opr1, opr2)
+                _ = il.set_reg(2, f'ER{oprs[0]}', d)
+                il.append(_)
+                _ = il.set_reg(1, f'R{oprs[1]}', m)
+
+            elif decoded.mnemonic == 'EXTBW':
+                opr1 = il.reg(1, f'ER{oprs[0]}')
+                _ = il.set_reg(2, f'ER{oprs[0]}', il.sign_extend(2, opr1))
+            
+            elif decoded.mnemonic == 'INC':
+                adr = il.reg(2, 'EA')
+                adr = dsr_prefix(adr)
+                _ = il.store(1, adr, il.add(1, il.load(1, adr), il.const(1, 1)))
+
+            elif decoded.mnemonic in ['L', 'ST']:
+                # Figure the value
+                if inst.first_operand == 'ERn':
+                    value = f'ER{oprs[0]}'
+                    sz = 2
+                elif inst.first_operand == 'QRn':
+                    value = f'QR{oprs[0]}'
+                    sz = 8
+                elif inst.first_operand == 'Rn':
+                    value = f'R{oprs[0]}'
+                    sz = 1
+                elif inst.first_operand == 'XRn':
+                    value = f'XR{oprs[0]}'
+                    sz = 4
+
+                # Figure the address
+                if inst.second_operand == '[EA]':
+                    adr = il.reg(2, 'EA')
+                elif inst.second_operand == '[EA+]':
+                    adr = il.reg(2, 'EA')
+                elif inst.second_operand == '[ERm]':
+                    adr = il.reg(2, f'ER{oprs[1]}')
+                elif inst.second_operand == 'Disp16[ERm]':
+                    base = il.const(2, oprs[1][0])
+                    reg = il.reg(2, f'ER{oprs[1][1]}')
+                    adr = il.add(2, base, reg)
+                elif inst.second_operand == 'Disp6[BP]':
+                    base = il.const(1, oprs[1][0])
+                    reg = il.reg(2, 'ER12')
+                    adr = il.add(2, base, reg)
+                elif inst.second_operand == 'Disp6[FP]':
+                    base = il.const(1, oprs[1][0])
+                    reg = il.reg(2, 'ER14')
+                    adr = il.add(2, base, reg)
+                elif inst.second_operand == 'Dadr':
+                    adr = il.const(1, oprs[1])
+                else:
+                    assert False, f"Invalid L operand {inst.second_operand}"
+
+                # Handle prefix
+                adr = dsr_prefix(adr)
+
+                # Handle opcode
+                if inst.mnemonic == 'L':
+                    _ = il.set_reg(sz, value, il.load(sz, adr))
+                else:
+                    _ = il.store(sz, adr, il.reg(sz, value))
+
+                # Handle increment EA
+                if inst.second_operand == '[EA+]':
+                    il.append(_)
+                    _ = il.set_reg(2, 'EA', il.add(2, il.reg(2, 'EA'), il.const(1, 1)))
+
+            elif decoded.mnemonic == 'LEA':
+                if inst.first_operand == '[ERm]':
+                    _ = il.set_reg(2, 'EA', il.reg(2, f'ER{oprs[0]}'))
+                elif inst.first_operand == 'Dadr':
+                    _ = il.set_reg(2, 'EA', il.const(2, oprs[0]))
+                elif inst.first_operand == 'Disp16[ERm]':
+                    _ = il.set_reg(2, 'EA', il.add(2, il.reg(2, f'ER{oprs[1]}'), il.const(2, oprs[0])))
+                else:
+                    assert False, f'LEA with invalid operand {inst.first_operand}'
+
+            elif decoded.mnemonic == 'MOV':
+                if inst.first_operand == 'PSW':
+                    log_warn('TODO: handle PSW properly')
+                    return None
+
+                elif inst.first_operand == 'ERn':
+                    if inst.second_operand == 'ERm':
+                        opr2 = il.reg(2, f'ER{oprs[1]}')
+                    elif inst.second_operand == '#imm7':
+                        opr2 = il.const(1, oprs[1])
+                    elif inst.second_operand == 'SP':
+                        opr2 = il.reg(2, 'SP')
+                    else:
+                        assert False, f'MOV ERn with invalid second operand {inst.second_operand}'
+                    _ = il.set_reg(2, f'ER{oprs[0]}', opr2)
+
+                elif inst.first_operand == 'Rn':
+                    if inst.second_operand == 'Rm':
+                        opr2 = il.reg(1, f'R{oprs[1]}')
+                    elif inst.second_operand == '#imm8':
+                        opr2 = il.const(1, oprs[1])
+                    else:
+                        assert False, f'MOV Rn with invalid second operand {inst.second_operand}'
+                    _ = il.set_reg(1, f'R{oprs[0]}', opr2)
+
+                elif inst.first_operand == 'SP':
+                    er = il.reg(2, f'ER{oprs[1]}')
+                    _ = il.set_reg(2, 'SP', er)
+
+                else:
+                    assert False, f'MOV with invalid operand {inst.first_operand}'
+
+            elif decoded.mnemonic == 'MUL':
+                opr1 = il.reg(1, f'R{oprs[0]}')
+                opr2 = il.reg(1, f'R{oprs[1]}')
+                _ = il.set_reg(2, f'ER{oprs[0]}', il.mult(2, opr1, opr2))
+
+            elif decoded.mnemonic == 'NEG':
+                r = il.reg(1, f'R{oprs[0]}')
+                _ = il.set_reg(1, f'R{oprs[0]}', il.neg_expr(1, r))
+
+            elif decoded.mnemonic == 'NOP':
+                _ = il.nop()
+
+            elif decoded.mnemonic == 'OR':
+                opr1 = il.reg(1, f'R{oprs[1]}')
+                if inst.second_operand == 'Rm':
+                    opr2 = il.const(1, f'R{oprs[1]}')
+                else:
+                    opr2 = il.const(1, oprs[1])
+                _ = il.set_reg(1, f'R{oprs[1]}', il.or_expr(1, opr1, opr2))
+
+            elif decoded.mnemonic == 'RC':
+                _ = il.set_flag('C', il.const(1, 0))
+
+            elif decoded.mnemonic == 'RT':
+                _ = il.ret(il.reg(3, 'LR'))
+
+            elif decoded.mnemonic == 'SC':
+                _ = il.set_flag('C', il.const(1, 1))
+
+            elif decoded.mnemonic == 'SUB':
+                opr1 = il.reg(1, f'R{oprs[0]}')
+                opr2 = il.reg(1, f'R{oprs[1]}')
+                _ = il.set_reg(1, f'R{oprs[0]}', il.sub(1, opr1, opr2))
+
+            elif decoded.mnemonic == 'SUBC':
+                opr1 = il.reg(1, f'R{oprs[0]}')
+                opr2 = il.reg(1, f'R{oprs[1]}')
+                _ = il.set_reg(1, f'R{oprs[0]}', il.sub_borrow(1, opr1, opr2, il.flag('C')))
+
+            elif decoded.mnemonic == 'TB':
+                if inst.first_operand == 'Dbitadr':
+                    val = il.load(1, il.const_pointer(2, oprs[0][0]))
+                else:
+                    val = il.reg(1, f'R{oprs[0][0]}')
+                _ = il.set_flag('Z', il.not_expr(1, il.test_bit(1, val, il.const(1, oprs[0][1]))))
+
+            elif decoded.mnemonic == 'XOR':
+                opr1 = il.reg(1, f'R{oprs[1]}')
+                if inst.second_operand == 'Rm':
+                    opr2 = il.const(1, f'R{oprs[1]}')
+                else:
+                    opr2 = il.const(1, oprs[1])
+                _ = il.set_reg(1, f'R{oprs[1]}', il.xor_expr(1, opr1, opr2))
+            
+            else:
+                _ = il.unimplemented()
+            
+            if _ is not None:
+                il.append(_)
+
+            return decoded.length
     
     # def convert_to_nop(self, data: bytes, addr: int = 0) -> Optional[bytes]:
     #     return super().convert_to_nop(data, addr)
